f7bd00263b38113e19cad395ea4f85e4210753dc
AmanChawla7777 <58990088+AmanChawla7777@users.noreply.github.com>
2020-12-15 22:43:47 +0530

    Add files via upload
    v1

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const authRoutes = require('./routes/authRoutes');
+const cookieParser = require('cookie-parser');
+const { requireAuth, checkUser } = require('./middleware/authMiddleware');
+
+const app = express();
+
+// middleware
+app.use(express.static('public'));
+app.use(express.json());
+app.use(cookieParser());
+
+// view engine
+app.set('view engine', 'ejs');
+
+// database connection
+const dbURI = 'mongodb+srv://useraman:useraman@amanchawlacluster-ykolf.mongodb.net/test?retryWrites=true&w=majority';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+  .then((result) => {
+    app.listen(3000);
+    console.log("db connected");
+  })
+  .catch((err) => console.log(err));
+
+// routes
+app.get('*', checkUser);
+app.get('/', (req, res) => res.render('home'));
+app.get('/content', requireAuth, (req, res) => res.render('content'));
+app.use(authRoutes);
